358727ee2957472be4ac788e9b905c7719904e03
Markevo5150 <marcoAlopez2@outlook.com>
2023-10-25 11:53:50 -0600

    More changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,44 @@
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <div className="main-container">
+        <div className="image"></div>
+        <div className="bottom">
+          <h1 className="title">Stay updated!</h1>
+          <p className="info">
+            Join 60,000+ products managers receiving monthly updates on:
+          </p>
+          <div className="productInfo">
+            <div className="product">
+              <img src="../assets/images/icon-list.svg" alt="listIcon" />
+              <p className="detail">
+                Product discovery and building what matters
+              </p>
+            </div>
+
+            <div className="product">
+              <img src="../assets/images/icon-list.svg" alt="listIcon" />
+              <p className="detail">
+                Measuring to ensure updates are a success.
+              </p>
+            </div>
+
+            <div className="product">
+              <img src="../assets/images/icon-list.svg" alt="listIcon" />
+              <p className="detail">And much more!</p>
+            </div>
+            <h3 className="emailTitle">Email address</h3>
+            <input
+              type="text"
+              placeholder="email@company.com"
+              className="emailBox"
+            />
+            <button className="subscribe">
+              Subscribe to monthly newsletter
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
